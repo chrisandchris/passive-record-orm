05259b00640171fb267e2eb1eb0d8c8721dcbacb
Christian Klauenbösch <christian@klit.ch>
2015-05-16 09:35:47 -0400

    added container to dependency provider

--- src/ChrisAndChris/Common/RowMapperBundle/Services/Model/ModelDependencyProvider.php
+++ src/ChrisAndChris/Common/RowMapperBundle/Services/Model/ModelDependencyProvider.php
@@ -5,18 +5,20 @@ use ChrisAndChris\Common\RowMapperBundle\Services\Logger\LoggerInterface;
 use ChrisAndChris\Common\RowMapperBundle\Services\Pdo\PdoLayer;
 use ChrisAndChris\Common\RowMapperBundle\Services\Pdo\RowMapper;
 use ChrisAndChris\Common\RowMapperBundle\Services\Query\Builder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @name ModelDependencyProvider
- * @version 1.0.0
- * @since v2.0.0
- * @package KlitCommon
+ * @version    1.0.1
+ * @since      v2.0.0
+ * @package    KlitCommon
  * @subpackage RowMapperBundle
- * @author Christian Klauenbösch <christian@klit.ch>
- * @copyright Klauenbösch IT Services
- * @link http://www.klit.ch
+ * @author     Christian Klauenbösch <christian@klit.ch>
+ * @copyright  Klauenbösch IT Services
+ * @link       http://www.klit.ch
  */
 class ModelDependencyProvider {
+
     /** @var PdoLayer the pdo class */
     private $PDO;
     /** @var RowMapper the row mapper */
@@ -27,13 +29,17 @@ class ModelDependencyProvider {
     private $Logger;
     /** @var Builder the query builder */
     private $Builder;
+    /** @var ContainerInterface the container */
+    private $Container;
 
-    function __construct(PdoLayer $PDO, RowMapper $mapper, ErrorHandler $ErrorHandler, LoggerInterface $Logger, Builder $Builder) {
+    function __construct(PdoLayer $PDO, RowMapper $mapper, ErrorHandler $ErrorHandler,
+                         LoggerInterface $Logger, Builder $Builder, ContainerInterface $Container = null) {
         $this->PDO = $PDO;
         $this->Mapper = $mapper;
         $this->ErrorHandler = $ErrorHandler;
         $this->Logger = $Logger;
         $this->Builder = $Builder;
+        $this->Container = $Container;
     }
 
     /**
@@ -69,5 +75,18 @@ class ModelDependencyProvider {
      */
     public function getBuilder() {
         return $this->Builder;
+    }
+
+    /**
+     * Get a parameter
+     *
+     * @param $name
+     * @return mixed|null null if container is not set
+     */
+    public function getParameter($name) {
+        if ($this->Container === null) {
+            return null;
+        }
+        return $this->Container->getParameter($name);
     }
 }
